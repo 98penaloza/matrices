b3d0e5fa01dfd4bcefc7abc7f8d66ff172f4f486
98penaloza <47442557+98penaloza@users.noreply.github.com>
2020-01-02 19:26:15 -0800

    update readme

--- Readme.txt
+++ Readme.txt
@@ -7,14 +7,39 @@ nor I plan to provide a "cheating tool". Instead I'm trying to develop a tool th
 knowledged acquered from linear algebra courses.
 
 ###
-CLASSES
+CLASSES and METHODS
 ###
     Row
-        A sequence of numerical values read from left to right.
+            A sequence of numerical values read from left to right. Its index start counting from 1 and it includes
+        the last index number.
 
-        A row is initialized as followed Row([...]). Row forcefully has to be initialized with an instance of
-            a sequence of ordered numerical values such as list, tuple. and the values in such data structure
-            has to be numerical values.
+        __init__
+            A row is initialized as followed Row([...]). Row forcefully has to be initialized with an instance of
+        a sequence of ordered numerical values such as list, tuple. and the values in such data structure
+        have to be numerical values.
+
+        dunder operators( __add__, __sub__, __mul__, __truediv__, __radd__, ...)
+            A row can be added, substracted, etc... with numerical values or ordered sequences of numerical values
+        with the same length. In the firts case, it returns a new instance of a Row and each element is addded, substracted, etc...
+        In the second case, a new instance of a Row is returned in which every element of each data structured is added, substracted, etc...
+
+        dunder methods (__iter__, __getitem__, __setitem__, __delitem__)
+            - When __iter__ is called, it returns a generator that yields a value in the instance object Row.
+            - When __getitem__ is called, it returns the entry position of the Row (index starts at 1, and includes the last index)
+            - When __setitem__ is called it mutates the entry number given for any value given
+            - __delitem__ will mutate any entry num so the numerical value becomes 0
+
+        get_row
+            Returns the sequence of numerical values as a list
+
+
+
+
+
+
+
+
+
 
 
 
--- matrices.py
+++ matrices.py
@@ -1,100 +1,5 @@
 import math
-
-
-class Row:
-    def __init__(self, row):
-        if type(row) not in (list, tuple):
-            raise AssertionError(f'{row} is not an instance of a sequancial ordered numbers. \n')
-        if False in [type(e) in (int, float) for e in row]:
-            raise AssertionError(f'{row} contains a non numerical value. \n')
-        self.row = [int(entry) if float(entry) == int(entry) else float(entry) for entry in row]
-
-    def __str__(self):
-        return '| ' + ' '.join([str(entry) for entry in self.row]) + ' |'
-
-    def __repr__(self):
-        return f"{type(self).__name__}({self.row})"
-
-    def __pos__(self):
-        return eval(repr(self))
-
-    def __neg__(self):
-        return eval(f'{type(self).__name__}(' + str([-entry for entry in self.row]) + ')')
-
-    def __add__(self, val):
-        if type(val) in [int, float]:
-            return eval(f'{type(self).__name__}(' + str([entry + val for entry in self.row]) + ')')
-        elif type(val) in (Row, Column, list, tuple):
-            if len(self) != len(val):
-                raise AssertionError(f'The length of {val} is inconsistent. \n')
-
-            return eval(f'{type(self).__name__}(' + str([e1 + e2 for e1, e2 in zip(self, val)]) + ')')
-
-    def __sub__(self, val):
-        return self + -val
-
-    def __rsub__(self, l_val):
-        return self - l_val
-
-    def __radd__(self, l_val):
-        return self + l_val
-
-    def __mul__(self, val):
-        if type(val) in (int, float):
-            return eval(f'{type(self).__name__}(' + str([val * entry for entry in self.row]) + ')')
-        elif type(val) in [list, Row]:
-            return eval(f'{type(self).__name__}(' + str([e1 * e2 for e1, e2 in zip(self, val)]) + ')')
-
-    def __rmul__(self, l_val):
-        return self * l_val
-
-    def __truediv__(self, val):
-        return eval(f'{type(self).__name__}(' + str([entry / val for entry in self.row]) + ')')
-
-    def __rtruediv__(self, l_val):
-        return eval(f'{type(self).__name__}(' + str([l_val / entry for entry in self.row]) + ')')
-
-    def __rmul__(self, l_val):
-        return self * l_val
-
-    def __iter__(self):
-        return iter(self.row)
-
-    def __getitem__(self, entry_num):
-        return (self.row[entry_num - 1])
-
-    def __delitem__(self, entry_num):
-        self.row[entry_num - 1] = 0
-
-    def __len__(self):
-        return len(self.row)
-
-    def __setitem__(self, entry, val):
-        self.row[entry - 1] = val
-
-    def get_unit(self, entry_num):
-        val = self[entry_num]
-        return self / val
-
-    def get_row(self):
-        return self.row
-
-    def add_entry(self, *entries):
-        for e in entries:
-            if type(e) not in (int, float):
-                raise AssertionError(f'{e} is not an instance of a numerical value. ')
-            self.row.append(e)
-
-
-class Column(Row):
-    def __str__(self):
-        maxi = max([len(str(e)) for e in self.row])
-        return '\n'.join(['|' + eval("'{" + f":^{maxi+2}" + "}'.format( str(e))") + '|' for e in self.row])
-
-    def get_column(self):
-        return self.row
-
-    del Row.get_row
+from Row_Col import Row, Column
 
 
 class Matrix:
@@ -352,6 +257,9 @@ class Matrix:
 
 if __name__ == '__main__':
     # tests
+    l = [1, 2, 3]
+    print(Row(l)[1:])
+
     m = Matrix.iMatrix(3)
     print([r for r in m.iter_cols()])
     pass
